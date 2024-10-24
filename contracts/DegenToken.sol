3bc3839ef4929aee22db941f4b6018abfc600f00
a yu <202110498@fit.edu.ph>
2024-10-24 17:20:26 +0000

    deploy degentoken

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -6,9 +6,44 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
 
+    mapping(address => string[]) ownedItems;
+    
+    event TokenRedeemed(address buyer, uint256 amount, string item_id);
+
     constructor() ERC20("Degen", "DGN") {}
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function decimals() override public pure returns (uint8) {
+        return 0;
+    }
+
+    // Burn tokens, anyone can burn their own tokens
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function redeem(string memory item_id, uint256 amount) public {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _burn(msg.sender, amount);
+
+        ownedItems[msg.sender].push(item_id);
+        
+        emit TokenRedeemed(msg.sender, amount, item_id);
+    }
+
+    function getOwnedItems() public view returns (string[] memory) {
+        return ownedItems[msg.sender];
+    }
+
+    function getBalance() external view returns (uint256) {
+        return this.balanceOf(msg.sender);
     }
 }
